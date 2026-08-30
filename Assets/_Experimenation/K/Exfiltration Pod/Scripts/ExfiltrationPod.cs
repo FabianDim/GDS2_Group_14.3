83595e00d0f7cabada231a2d49c127f1df17858f
student-mel <melonym1101@gmail.com>
2026-08-30 20:03:12 +1000

    As a developer, I want to alter the Infiltration Pod logic to be Fusion-compatible so that its behaviour can be synced across the network for both players.
    First pass at converting Infiltration Pod code for Fusion. WIP testing, currently does not work right but doing an early commit first

--- Assets/_Experimenation/K/Exfiltration Pod/Scripts/ExfiltrationPod.cs
+++ Assets/_Experimenation/K/Exfiltration Pod/Scripts/ExfiltrationPod.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using _Experimenation.K.Event_Bus;
 using _Experimenation.K.Event_Bus.Events;
+using Fusion;
 using UnityEngine;
 
 namespace _Experimenation.K.Exfiltration_Pod.Scripts
 {
-    public class ExfiltrationPod : MonoBehaviour
+    public class ExfiltrationPod : NetworkBehaviour
     {
         [SerializeField] private float distanceFromRunner = 10f;
         [SerializeField] private int pointTarget = 100;
-        private GameObject _runner;
+        private NetworkObject _runner;  // swapped to network obj
         private bool _conditionCleared;
         private bool _runnerReachedPod;
         private GameObject _pod;
@@ -19,16 +20,35 @@ namespace _Experimenation.K.Exfiltration_Pod.Scripts
         [SerializeField] private float podUnavailableTime = 15f;
         private WaitForSeconds _podUnavailableTime;
         
+        [Networked] private TickTimer PodTimer { get; set; }
+        [Networked] private bool PodActive { get; set; }
+        
         private void Awake()
         {
-            _runner = GameObject.FindGameObjectWithTag("Runner");
+            //_runner = GameObject.FindGameObjectWithTag("Runner");
             _pod = transform.GetChild(0).gameObject;
             _pod.SetActive(false);
-            _podAvailableTime = new WaitForSeconds(podAvailableTime);
-            _podUnavailableTime = new WaitForSeconds(podUnavailableTime);
+            /*_podAvailableTime = new WaitForSeconds(podAvailableTime);
+            _podUnavailableTime = new WaitForSeconds(podUnavailableTime);*/
             
             EventBus.Subscribe<TimeRunsOutEvent>(OnTimeRunsOut);
             EventBus.Subscribe<TokenCollectedEvent>(OnTokenCollected);
+        }
+        
+        public override void Spawned()
+        {
+            if (!Object.HasStateAuthority) return;
+
+            // find the runner network object 
+            foreach (var player in Runner.ActivePlayers)
+            {
+                var obj = Runner.GetPlayerObject(player);
+                if (obj != null && obj.CompareTag("Runner"))
+                {
+                    _runner = obj;
+                    break;
+                }
+            }
         }
 
         private void OnDestroy()
@@ -36,19 +56,74 @@ namespace _Experimenation.K.Exfiltration_Pod.Scripts
             EventBus.Unsubscribe<TimeRunsOutEvent>(OnTimeRunsOut);
             EventBus.Unsubscribe<TokenCollectedEvent>(OnTokenCollected);
         }
+        
+        public override void FixedUpdateNetwork()
+        {
+            if (Object.HasStateAuthority)
+            {
+                // converted pod coroutine behaviour 
+                if (!_conditionCleared) return;
 
-        private void OnTriggerEnter(Collider other)
+                if (PodActive)
+                {
+                    if (PodTimer.Expired(Runner))
+                    {
+                        PodActive = false;
+                        PodTimer = TickTimer.CreateFromSeconds(Runner, podUnavailableTime);
+                        Debug.Log($"Pod deactivated for {podUnavailableTime}s.");
+                    }
+                    
+                    if (RunnerReachedPod())
+                    {
+                        EventBus.Raise(new RoundOverEvent(true));
+                        _runnerReachedPod = true;
+                        Debug.Log($"Runner reached pod.");
+                    }
+                }
+                else
+                {
+                    if (PodTimer.Expired(Runner))
+                    {
+                        SpawnPod();
+                        PodActive = true;
+                        PodTimer = TickTimer.CreateFromSeconds(Runner, podAvailableTime);
+                    }
+                }
+            }
+
+            _pod.SetActive(PodActive);
+        }
+
+        /*private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Runner")) return;
             EventBus.Raise(new RoundOverEvent(true));
+        }*/
+
+        // using a distance check instead of OnTriggerEnter to avoid using fusion physics
+        bool RunnerReachedPod()
+        {
+            if (_runner == null) return false;
+            return Vector3.Distance(_runner.transform.position, transform.position) < 2f;
+        }
+        
+        private void SpawnPod()
+        {
+            if (_runner == null) return;
+
+            var spawnPos = _runner.transform.position + _runner.transform.forward * distanceFromRunner;
+            transform.position = spawnPos;
+            Debug.Log($"Pod activated at {spawnPos} for {podAvailableTime}s.");
         }
         
         #region Event Bus Handlers
         private void OnTimeRunsOut(TimeRunsOutEvent ev)
         {
             if(_conditionCleared) return;
-            StartCoroutine(SpawnPod());
+            //StartCoroutine(SpawnPod());
             _conditionCleared = true;
+            PodTimer = TickTimer.CreateFromSeconds(Runner, podAvailableTime);
+            PodActive = true;
         }
 
         private void OnTokenCollected(TokenCollectedEvent ev)
@@ -56,11 +131,13 @@ namespace _Experimenation.K.Exfiltration_Pod.Scripts
             if(_conditionCleared) return;
             pointTarget -= ev.Points;
             if (pointTarget > 0) return;
-            StartCoroutine(SpawnPod());
+            //StartCoroutine(SpawnPod());
             _conditionCleared = true;
+            PodTimer = TickTimer.CreateFromSeconds(Runner, podAvailableTime);
+            PodActive = true;
         }
 
-        private IEnumerator SpawnPod()
+        /*private IEnumerator SpawnPod()
         {
             if(_conditionCleared) yield break;
             while (!_runnerReachedPod)
@@ -72,7 +149,7 @@ namespace _Experimenation.K.Exfiltration_Pod.Scripts
                 _pod.SetActive(false);
                 yield return _podUnavailableTime;
             }
-        }
+        }*/
         #endregion
     }
 }
